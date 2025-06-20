3066794fa6ebf397c67dcf5efcac41493b89101b
Amjad-Hashmi <hashmiamjadpak@gmail.com>
2025-06-20 11:01:56 +0500

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { Container, Edit, Trash2 } from "lucide-react";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
+    ///// Header
+
+    <div  className=" bg-page-bg-light-blue w-full h-screen">
+
+    <div className="text-center ">
+
+      <h1 className="text-4xl font-bold pt-8">Todo App</h1>
+
+      <p className="p-2">stay organized and get thing done</p>
+
+    </div>
+
+    </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // <Edit/>
+      // <Trash2 />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+       "page-bg-light-blue": "#edf2fb",
+       "add-btn-bright-blue": "#3b82f6",
+       "input-b-light-gray": "#e5e7eb",
+       "total-count-box": "#f3f4f6",
+       "completed-box": "#d1fae5",
+       "remaining-box": "#fde68a",
+       "active-tab" : "#2563eb",
+       "inactive-tab": "#e5e7eb",
+       "text-main-color": "#374151",
+       "muted-text": "#9ca3af",
+       "card-bg": "#fff"
+      },
+
+    },
   },
   plugins: [],
 }

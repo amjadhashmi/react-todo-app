94ed471de089979be02722826be44791f4616a74
Amjad-Hashmi <hashmiamjadpak@gmail.com>
2025-06-20 11:41:55 +0500

    add and setup input div and button

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import { Container, Edit, Trash2 } from "lucide-react";
 export default function App() {
   return (
 
-    ///// Header
+     <div  className=" bg-page-bg-light-blue w-full h-screen">
 
-    <div  className=" bg-page-bg-light-blue w-full h-screen">
 
-    <div className="text-center ">
+         {/*//////////////////////// Header///////////////////////////////// */}
 
-      <h1 className="text-4xl font-bold pt-8">Todo App</h1>
+          <div className="text-center ">
 
-      <p className="p-2">stay organized and get thing done</p>
+            <h1 className="text-4xl font-bold pt-8">Todo App</h1>
 
-    </div>
+            <p className="p-2">stay organized and get thing done</p>
+
+          </div>
+
+
+
+        {/*/////////////////////////////// Input Div /////////////////////////////////*/}
+
+        <div className="w-2/3 h-28 bg-white m-auto flex rounded-md mt-7">
+
+          <input type="text" className="bg-white p-3 mr-2 w-full h-12 mt-7 ml-7 rounded-md border border-gray-300 " placeholder="What needs to be done?" />
+          <button className="bg-add-btn-bright-blue text-white p-1 rounded-md w-20 h-12 mt-7 mr-7">Add</button>
+
+        </div>
+
 
     </div>
   )
